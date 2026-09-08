568279fea25a4dfc697e4fdc6135f61467e7257e
szinkovsky <zink.sergei97@gmail.com>
2026-09-08 21:27:15 +0300

    add: migrations

---
+++ migrations/000001_create_users.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+);
